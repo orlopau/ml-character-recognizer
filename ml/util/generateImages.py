9be26cfd46954d8af33c812ecaa3636f3e87b920
Paul Orlob <orlobpaul@gmail.com>
2019-11-24 19:00:22 +0100

    Added image preprocessor
    The preprocessor adjusts images to have a square format, and normalizes the coordinates.

--- ml/util/generateImages.py
+++ ml/util/generateImages.py
@@ -1,17 +1,9 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
-from util.parseCSV import CSVParser
+from util.parseCSV import *
 from PIL import Image, ImageDraw
 import os
 
-img = Image.new("1", (40, 40), color="white")
-
-draw = ImageDraw.Draw(img)
-
-draw.line([(0, 0), (40, 40)], fill="black", width=1)
-
-img.save("test.png")
-
-parser = CSVParser("data0.csv", scale=40)
+parser = CSVParser("data0.csv")
 
 while True:
     data = parser.next_line()
@@ -25,10 +17,12 @@ while True:
         except FileExistsError:
             pass
 
-        img = Image.new('1', (40, 40), color='white')
+        normalized_paths = preprocess_paths(data['paths'], data['maxX'], data['minX'], data['maxY'], data['minY'], scale=28)
+
+        img = Image.new('1', (28, 28), color='white')
         draw = ImageDraw.Draw(img)
 
-        for line in data['paths']:
+        for line in normalized_paths:
             draw.line(line)
 
         img.save(os.path.join(path, str(len(os.listdir(path)))) + ".png")
--- ml/util/parseCSV.py
+++ ml/util/parseCSV.py
@@ -2,9 +2,8 @@ import csv
 
 
 class CSVParser:
-    def __init__(self, path: str, scale: float):
+    def __init__(self, path: str):
         self.f = open(path, "U")
-        self.scale = scale
         self.csv_reader = csv.reader(self.f, delimiter=",")
 
     def next_line(self):
@@ -18,19 +17,42 @@ class CSVParser:
 
     def parse_line(self, line: []):
 
-        data = {
-            'char': line.pop(0),
-            'paths': []
-        }
+        # noinspection PyDictCreation
+        data = {'uid': '',
+                'char': '',
+                'paths': [],
+                'minX': float('inf'),
+                'maxX': -float('inf'),
+                'minY': float('inf'),
+                'maxY': -float('inf')}
+
+        data['uid'] = line.pop(0)
+        data['char'] = line.pop(0)
 
         current_path = []
+
+        current_coord_pair = []
 
         while len(line) > 0:
             char = line.pop(0)
             if char == ",":
-                pass
+                raise Exception('invalid state!')
             elif CSVParser.is_number(char):
-                current_path.append(float(char) * self.scale)
+                value = float(char)
+
+                current_coord_pair.append(value)
+
+                if len(current_coord_pair) == 1:
+                    data['minX'] = min(data['minX'], value)
+                    data['maxX'] = max(data['maxX'], value)
+                elif len(current_coord_pair) == 2:
+                    data['minY'] = min(data['minY'], value)
+                    data['maxY'] = max(data['maxY'], value)
+
+                if len(current_coord_pair) == 2:
+                    current_path.append((current_coord_pair[0], current_coord_pair[1]))
+                    current_coord_pair = []
+
             elif char == "x":
                 data['paths'].append(current_path)
                 current_path = []
@@ -44,3 +66,54 @@ class CSVParser:
         except ValueError:
             return False
         return True
+
+
+def calc_center_of_mass(paths):
+    sum_x = 0
+    sum_y = 0
+
+    len_coords = 0
+
+    for path in paths:
+        len_coords += len(path)
+        for coord in path:
+            sum_x += coord[0]
+            sum_y += coord[1]
+
+    return [sum_x / len_coords, sum_y / len_coords]
+
+
+def preprocess_paths(paths, max_x, min_x, max_y, min_y, scale):
+    width = max_x - min_x
+    height = max_y - min_y
+
+    g_max = max(max_x, max_y)
+    g_min = min(min_x, min_y)
+
+    x_increase = 0
+    y_increase = 0
+
+    # fix for character 'I'
+    if height > width:
+        x_increase = height - width
+    else:
+        y_increase = width - height
+
+    # normalize all values
+    normalized_paths = []
+
+    for path in paths:
+        normalized_path = []
+        for coords in path:
+            normalized_coords = (
+                normalize(coords[0] + x_increase / 2, min_x, max_x + x_increase) * scale,
+                normalize(coords[1] + y_increase / 2, min_y, max_y + y_increase) * scale
+            )
+            normalized_path.append(normalized_coords)
+        normalized_paths.append(normalized_path)
+
+    return normalized_paths
+
+
+def normalize(val, min_val, max_val):
+    return (val - min_val) / (max_val - min_val)
