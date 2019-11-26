ddc14a39c43c803da985f4a9dcb853da753958fe
Paul Orlob <orlobpaul@gmail.com>
2019-11-26 19:30:26 +0100

    Added mnist model

---
+++ ml/learning/mnist.py
@@ -0,0 +1,48 @@
+from __future__ import print_function
+import tensorflow as tf
+
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+x_train = tf.expand_dims(x_train, 3)
+x_test = tf.expand_dims(x_test, 3)
+
+y_train = tf.keras.utils.to_categorical(y_train, num_classes)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes)
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Conv2D(32, kernel_size=(3, 3),
+                                 activation='relu',
+                                 input_shape=(28, 28, 1)))
+model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.Dropout(0.25))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(num_classes, activation='softmax'))
+
+model.compile(loss=tf.keras.losses.categorical_crossentropy,
+              optimizer=tf.keras.optimizers.Adam(),
+              metrics=['accuracy'])
+
+print(model.summary())
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+
+score = model.evaluate(x_test, y_test, verbose=0)
+
+print("Saving weights...")
+model.save('./mnist_cnn.h5')
+
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
