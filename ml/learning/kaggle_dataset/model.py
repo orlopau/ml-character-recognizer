296c881d78702e9cb2993afdaafaf9a67f98b602
Paul Orlob <orlobpaul@gmail.com>
2019-12-02 00:05:59 +0100

    Fixed model by dividing data by 255

--- ml/learning/kaggle_dataset/model.py
+++ ml/learning/kaggle_dataset/model.py
@@ -1,5 +1,4 @@
 import pandas
-import numpy as np
 from learning.models import v1
 
 df = pandas.read_csv('dataset.csv').astype('float32')
@@ -8,9 +7,11 @@ df = df.sample(frac=1).reset_index(drop=True)
 X = df.drop(df.columns[0], axis=1)
 y = df['0']
 
-data = X.to_numpy().reshape((len(X)), 28, 28)
+data = X.to_numpy().reshape((len(X)), 28, 28) / 255.
 
 validation_split = 0.75
 validation_index = int(round(len(data) * validation_split))
+
+print("Should show a " + str(y[14]))
 
 v1.learn(data[:validation_index], y[:validation_index], data[validation_index:], y[validation_index:], epochs=3)
